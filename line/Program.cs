f1504fdd31b556f80b7ef29ce6fa19db3329ac6c
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-06 21:47:34 +0530

    Welcome to Line Comparison Program

---
+++ line/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Program");
+            Class1.LineComputation();
+            Class2.LineComparison();    
+            Class3.LineEquality(); 
+            Class4.ComparisonMethod();  
+        }
+    }
+}
